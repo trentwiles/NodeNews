065d02284e8ddb12afa36ed35d1bffbd2bc6cdca
trentwiles <wilest7@gmail.com>
2024-07-15 19:24:16 -0400

    enable EJS

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const db = require('./dbase')
 
+app.set('view engine', 'ejs');
+
 db.init()
 
 app.get('/', function (req, res) {
+  res.render('index', {title: "Home"})
+})
+
+app.post('/subscribe', function (req, res) {
   res.send('Hello World')
 })
 
