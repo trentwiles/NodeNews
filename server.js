2e60c73524dbb063964be26622186a12bd0ca3e0
trentwiles <wilest7@gmail.com>
2024-07-16 21:38:10 -0400

    future skeleton

--- server.js
+++ server.js
@@ -7,20 +7,28 @@ app.set('view engine', 'ejs');
 db.init()
 
 app.get('/', function (req, res) {
-  res.render('index', {title: "Home"})
+  res.render('index', {title: "Home", index: true})
 })
 
 app.post('/subscribe', function (req, res) {
   // maybe add recaptcha here?
   var email = req.body.id
   // temp captcha mock code
-  var captchaStatus = (1=1)
+  var captchaStatus = true
   if(captchaStatus && email != null){
     db.insertEmail(email)
-    res.send('It works!')
+    res.redirect("/thanks")
   }else{
     res.send('Invalid')
   }
+})
+
+app.get('/thanks', function (req, res){
+  res.render('thanks', {title: "Thank You", index: false})
+})
+
+app.get('/unsubscribe', function(req, res){
+  res.render('unsub', {"title": "Unsubcribe", index: false})
 })
 
 app.listen(3000)
